7dde6f702c36b7b4151d0f65058c6ecb8e134eca
bolijesus <44576401+bolijesus@users.noreply.github.com>
2022-01-27 16:09:14 -0500

    Rediret https to http

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,4 +1,11 @@
+window.onload=function(){
+     $(function(){
+         if(window.location.protocol==="https:")
+             window.location.protocol="http";
+     });
+ }
 $(document).ready(function () {
+    
     $('.descargar-ajax').on('click', function () {
         
        let token = $('input[name=_token]').val(); 
